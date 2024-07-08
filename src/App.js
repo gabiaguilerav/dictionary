a70a71135b31501b7830a1443bc0d2388ec602ac
Gabriela Aguilera <160552031+gabiaguilerav@users.noreply.github.com>
2024-07-08 11:38:01 -0600

    Basic HTML structure added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Dictionary app</h1>
+        <h2>
+          We can help you finding a word{" "}
+          <span class="material-symbols-outlined">captive_portal</span>
+        </h2>
+
+        <footer>
+          Coded by Gabriela Aguilera. Open-sourced on Github and hosted on
+          Netlify
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
