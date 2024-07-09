cd82e4e91abd507c541d51c59e62766eeee1bed2
Gabriela Aguilera <160552031+gabiaguilerav@users.noreply.github.com>
2024-07-09 11:39:22 -0600

    API meaning interaction done

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
     // Documentation https://api.dictionaryapi.dev/api/v2/entries/en/<word>
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -36,6 +37,8 @@ export default function Dictionary() {
         />
         <input type="submit" id="searcher" />
       </form>
+
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Meaning.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+
+  return (
+    <div className="Meaning">
+      <h4>{props.meaning.partOfSpeech}</h4>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>{definition.definition}</p>
+            <em>
+              <p>{definition.example}</p>
+            </em>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
